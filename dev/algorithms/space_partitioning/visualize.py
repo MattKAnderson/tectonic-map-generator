3abc14111ec7573520ef57f316cd184c85cd5e91
Matt <matt.anderson0987@gmail.com>
2024-03-21 22:08:09 -0400

    refactoring, color logic, add logic to better display unlabelled boundaries

--- dev/algorithms/space_partitioning/visualize.py
+++ dev/algorithms/space_partitioning/visualize.py
@@ -1,6 +1,7 @@
 import json
 import random
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolor
 
 
 def plot_border(ax, xsize, ysize):
@@ -8,73 +9,138 @@ def plot_border(ax, xsize, ysize):
     ys = [0, 0, ysize, ysize, 0]
     ax.plot(xs, ys, c="black")
 
-fig, ax = plt.subplots(2, 2)
+
+def not_adjacent(p1, p2):
+    return abs(p1[0] - p2[0]) > 1 or abs(p1[1] - p2[1]) > 1
+
+
+def split_on_disconnect(split_lines, p1, p2):
+    if not_adjacent(p1, p2):
+        split_lines.append([p2])
+    else:
+        split_lines[-1].append(p2)
+
+
+def separate_travel_history_lines(traversal_data):
+    traversal_split_lines = [[traversal_data[0][0]]]
+    split_on_disconnect(traversal_split_lines, traversal_split_lines[0][0], traversal_data[0][1])
+    for i in range(1, len(traversal_data)):
+        split_on_disconnect(traversal_split_lines, traversal_split_lines[-1][-1], traversal_data[i][0])
+        split_on_disconnect(traversal_split_lines, traversal_data[i][0], traversal_data[i][1])
+    return traversal_split_lines
+
+
+def plot_edges(ax, traversal_data):
+    for edge in traversal_data:
+        if not_adjacent(edge[0], edge[1]):
+            continue
+        xs = [p[1] for p in edge]
+        ys = [p[0] for p in edge]
+        ax.plot(xs, ys, c="red")
+
+
+def plot_region_map(ax, region_data):
+    ax.matshow(region_data)
+
+
+# function returns a color for each line, such that at each intersection
+# no two lines have the same color. 
+def select_colors(split_lines):
+    # these could be tuned to amke the plot nicer, but specifically 
+    # choosing colors is much better than using a colormap
+    css_named_colors = [
+        "gray", "lightcoral", "darkred", "chocolate",
+        "orange", "tan", "darkkhaki", "yellow", "chartreuse",
+        "forestgreen", "aquamarine", "lightseagreen", "darkcyan",
+        "royalblue", "navy", "slateblue", "blueviolet", "indigo",
+        "violet",  "magenta"
+    ]
+    ncolor = 20
+    # cmap = plt.get_cmap("tab20", 20)
+    counter = 0
+    interesting_vertices = set()
+    vertex_to_lines = {}
+    for i in range(len(split_lines)):
+        for j in range(len(split_lines[i])):
+            p = (split_lines[i][j][0], split_lines[i][j][1])
+            if p in vertex_to_lines:
+                interesting_vertices.add(p)
+                vertex_to_lines[p].append(i)
+            else:
+                vertex_to_lines[p] = [i]
+
+    line_intersections = [[] for i in range(len(split_lines))]
+    for v in interesting_vertices:
+        for i in set(vertex_to_lines[v]):
+            line_intersections[i].append(v)
+    
+    intersections = {}
+    chosen_color_ids = []
+    for i in range(len(split_lines)):
+        intersecting_colors = []
+        for v in line_intersections[i]:
+            if v in intersections:
+                intersecting_colors.extend(intersections[v])
+            else:
+                intersections[v] = []
+        color_id = random.randint(0, ncolor-1)
+        while (color_id in intersecting_colors):
+            counter += 1
+            color_id = random.randint(0, ncolor-1)
+        chosen_color_ids.append(color_id)
+        for v in line_intersections[i]:
+            intersections[v].append(color_id)
+
+    print(f"Stopped {counter} instances of color clashing")
+
+    # return [
+    #    cmap(cid/ncolor) for cid in chosen_color_ids
+    #]
+    return [
+        mcolor.CSS4_COLORS[css_named_colors[cid]] for cid in chosen_color_ids
+    ]
+
+
+def plot_colored_borderlines(ax, borderlines_data):
+    all_split_lines = []
+    for i, line in enumerate(borderlines_data):
+        split_lines = [[line[0]]]
+        for p in line[1:]:
+            split_on_disconnect(split_lines, split_lines[-1][-1], p)
+        all_split_lines.extend(split_lines)
+    colors = select_colors(all_split_lines)
+    for sl, c in zip(all_split_lines, colors):
+        xs = [e[1] for e in sl]
+        ys = [e[0] for e in sl]
+        ax.plot(xs, ys, c=c)
+
 
 f = open("build/dev/algorithms/space_partitioning/region_partitions.json", "r")
-data = json.load(f)["matrix"]
-xsize = len(data)
-ysize = len(data[0])
+region_data = json.load(f)["matrix"]
+fig, ax = plt.subplots(1, 3)
+xsize = len(region_data)
+ysize = len(region_data[0])
+plot_region_map(ax[0], region_data)
 
-ax[0][0].matshow(data)
 
 f = open("build/dev/algorithms/space_partitioning/border_map.json", "r")
-data = json.load(f)["matrix"]
+# border_map_data = json.load(f)["matrix"]
+# ax[0][1].matshow(border_map_data)
 
-ax[0][1].matshow(data)
 
 f = open("build/dev/algorithms/space_partitioning/traversal_history.json", "r")
 traversal_data = json.load(f)["vector"]
-
 print(f"Number of nodes touched: {len(traversal_data)}")
+plot_edges(ax[1], traversal_data)
+plot_border(ax[1], xsize, ysize)
 
-traversal_split_lines = [[traversal_data[0]]]
-for i in range(1, len(traversal_data)):
-    x1 = traversal_data[i-1][0]
-    y1 = traversal_data[i-1][1]
-    x2 = traversal_data[i][0]
-    y2 = traversal_data[i][1]
-    if (abs(x1 - x2) > 1 or abs(y1 - y2) > 1):
-        traversal_split_lines.append([traversal_data[i]])
-    else:
-        traversal_split_lines[-1].append(traversal_data[i])
-
-for line in traversal_split_lines:
-    xs = [e[1] for e in line]
-    ys = [e[0] for e in line]
-    ax[1][0].plot(xs, ys, c="red")
-
-plot_border(ax[1][0], xsize, ysize)
 
 f = open("build/dev/algorithms/space_partitioning/borderlines.json", "r")
-data = json.load(f)["matrix"]
+borderlines_data = json.load(f)["matrix"]
+print(f"num lines: {len(borderlines_data)}")
+plot_colored_borderlines(ax[2], borderlines_data)
+plot_border(ax[2], xsize, ysize)
 
-print(f"num lines: {len(data)}")
-
-cmap = plt.colormaps["hsv"]
-for i, line in enumerate(data):
-    print(f"Vertices in line: {len(line)}")
-    cc = cmap(random.random())
-    split_lines = [[line[0]]]
-    for p in line[1:]:
-        x1 = p[0]
-        y1 = p[1]
-        x2 = split_lines[-1][-1][0]
-        y2 = split_lines[-1][-1][1]
-        if (abs(x1 - x2) > 1 or abs(y1 - y2) > 1):
-            split_lines.append([p])
-        else:
-            split_lines[-1].append(p)
-
-    for sl in split_lines:
-        xs = [e[1] for e in sl]
-        ys = [e[0] for e in sl]
-        ax[1][1].plot(xs, ys, c=cc)
-    # plt.text(line[-1][1], line[-1][0], f"line {i}")
-
-ax[1][1].invert_yaxis()
-ax[1][0].invert_yaxis()
-#plot_border(ax[1][1], xsize, ysize)
-# plt.legend()
+ax[2].invert_yaxis()
+ax[1].invert_yaxis()
 plt.show()
-
-# TODO: implement logic so that touching lines all have different colors

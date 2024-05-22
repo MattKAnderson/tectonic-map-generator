57a97335b28d9e948e11d8453c2c8f1f6b6f3b3f
Matt <matt.anderson0987@gmail.com>
2024-05-21 22:32:04 -0400

    Add method for plotting the hull and centroid of a single region

--- dev/algorithms/voronoi_diagram/visualize_fortunes_algo.py
+++ dev/algorithms/voronoi_diagram/visualize_fortunes_algo.py
@@ -30,24 +30,37 @@ def plot_bounds(ax, xmin, xmax, ymin, ymax, color="r"):
     ax.plot(xs, ys, color=color)
 
 
+def plot_region(ax, region, bounds_color="r", centroid_color="b"):
+    xs = [p[0] for p in region["vertices"]] + [region["vertices"][0][0]]
+    ys = [p[1] for p in region["vertices"]] + [region["vertices"][0][1]]
+    ax.plot(xs, ys, color=bounds_color)
+    ax.scatter(region["centroid"][0], region["centroid"][1], color=centroid_color)
+
+
 fig, ax = plt.subplots(1)
-f1 = open("build/dev/algorithms/voronoi_diagram/vertex_line_segments.json")
-f2 = open("build/dev/algorithms/voronoi_diagram/diagram_seeds.json")
-f3 = open("build/dev/algorithms/voronoi_diagram/region_adjacency_line_segments.json")
+base_dir = "build/dev/algorithms/voronoi_diagram/"
+#f1 = open("vertex_line_segments.json")
+#f2 = open("diagram_seeds.json")
+f1 = open(base_dir + "vertex_line_segments.json")
+f2 = open(base_dir + "diagram_seeds.json")
+f3 = open(base_dir + "region_adjacency_line_segments.json")
+f4 = open(base_dir + "regions.json")
 line_segments = json.load(f1)["vector"]
 region_connections = json.load(f3)["vector"]
 diagram_seeds = json.load(f2)["vector"]
+regions = json.load(f4)["vector"]
 
-
-plot_line_segments(ax, line_segments)
-# plot_line_segments(ax, region_connections, "b")
-plot_points(ax, diagram_seeds, "b")
+plot_line_segments(ax, line_segments, "r")
+plot_line_segments(ax, region_connections, "b")
+#plot_points(ax, diagram_seeds, "b")
+#for i in range(int(len(regions) / 8)):
+#    plot_region(ax, regions[i])
 #plot_bounds(ax, xmin, xmax, ymin, ymax)
 #plot_points(ax, manual_points, "r")
 #for p in sorted(diagram_seeds, key=lambda x: x[0]):
 #    print(p)
 # print(len(diagram_seeds))
 
-ax.set_xlim(-200.0, 4296.0)
-ax.set_ylim(-200.0, 4296.0)
+ax.set_xlim(-100.0, 4196.0)
+ax.set_ylim(-100.0, 4196.0)
 plt.show()

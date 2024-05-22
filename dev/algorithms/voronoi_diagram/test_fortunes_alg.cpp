410b2b3067bd9c2a302a1b6622627b95fbe26a2a
Matt <matt.anderson0987@gmail.com>
2024-05-21 22:30:11 -0400

    add methods for outputting region adjacencies and hulls to json

--- dev/algorithms/voronoi_diagram/test_fortunes_alg.cpp
+++ dev/algorithms/voronoi_diagram/test_fortunes_alg.cpp
@@ -40,12 +40,39 @@ std::vector<std::pair<RealCoordinate, RealCoordinate>> graph_to_line_segments(
     return line_segments;
 }
 
+std::vector<std::pair<RealCoordinate, RealCoordinate>> region_adjacency_line_segments(
+    RegionGraph& graph
+) {
+    std::vector<std::pair<RealCoordinate, RealCoordinate>> line_segments;
+    std::unordered_set<RegionNode*> visited;
+    for (RegionNode* region : graph.get_regions()) {
+        for (RegionNode* adj_region : region->adjacent) {
+            if (visited.find(adj_region) != visited.end()) { continue; }
+            line_segments.emplace_back(region->centroid(), adj_region->centroid());
+        }
+        visited.insert(region);
+    }
+    return line_segments;
+}
+
+std::string to_json(RegionNode* p) {
+    std::string centroid = to_json(p->centroid());
+    std::string vertices_string = "[";
+    for (const RealCoordinate& vertex : p->vertices) {
+        vertices_string += to_json(vertex) + ", ";
+    }
+    vertices_string.pop_back(); 
+    vertices_string.pop_back();
+    vertices_string += "]";
+    return "{\"centroid\": " + centroid + ", \"vertices\": " + vertices_string + "}";
+}
+
 int main() {
 
     int seed = 12490; 
     int xsize = 4096;
     int ysize = xsize;
-    int nseeds = 100;
+    int nseeds = 1000;
 
     VoronoiDiagram voronoi_diagram(seed);
     auto t1 = std::chrono::high_resolution_clock::now();
@@ -57,17 +84,18 @@ int main() {
 
     std::vector<RealCoordinate> seeds = voronoi_diagram.get_seeds();
     VertexGraph vgraph = voronoi_diagram.get_vertex_graph();
-    //RegionGraph rgraph = voronoi_diagram.get_region_graph();
+    RegionGraph rgraph = voronoi_diagram.get_region_graph();
     std::cout << "Finished getting vertices and region graphs" << std::endl;
     typedef std::vector<std::pair<RealCoordinate, RealCoordinate>> ls_vector;
     ls_vector vertex_line_segments = graph_to_line_segments(vgraph.get_vertices());
-    //ls_vector region_adjacency_segments = graph_to_line_segments(rgraph);
-    
+    ls_vector region_adjacency_segments = region_adjacency_line_segments(rgraph);
+    std::vector<RegionNode*> regions = rgraph.get_regions();
     std::cout << "Writing results" << std::endl;
     vector_output("vertex_line_segments.json", vertex_line_segments);
-    //vector_output(
-    //    "region_adjacency_line_segments.json", region_adjacency_segments
-    //);
+    vector_output(
+        "region_adjacency_line_segments.json", region_adjacency_segments
+    );
+    vector_output("regions.json", regions);
     vector_output("diagram_seeds.json", seeds);
     std::cout << "Done" << std::endl;
 }

889c5e42539381a40d9ac57e07b0002a8d9c3f95
Matt <matt.anderson0987@gmail.com>
2024-05-22 15:16:28 -0400

    commented out  all currently unused functions

--- dev/algorithms/voronoi_diagram/geometry.hpp
+++ dev/algorithms/voronoi_diagram/geometry.hpp
@@ -10,35 +10,35 @@
 
 double euclidean_distance(const RealCoordinate& c1, const RealCoordinate& c2);
 
-double toroidal_distance(
-    Coordinate& c1, Coordinate& c2, int xsize, int ysize
-);
+//double toroidal_distance(
+//    Coordinate& c1, Coordinate& c2, int xsize, int ysize
+//);
 
-bool is_between(double x, double a, double b);
+//bool is_between(double x, double a, double b);
 
-RealCoordinate triangle_centroid(
-    const RealCoordinate& a, const RealCoordinate& b, const RealCoordinate& c
-);
+//RealCoordinate triangle_centroid(
+//    const RealCoordinate& a, const RealCoordinate& b, const RealCoordinate& c
+//);
 
 RealCoordinate triangle_circumcenter(
     const RealCoordinate& a, const RealCoordinate& b, const RealCoordinate& c
 );
 
-RealCoordinate lines_intercept(
-    const RealCoordinate& l1a, const RealCoordinate& l1b, 
-    const RealCoordinate& l2a, const RealCoordinate& l2b
-);
+//RealCoordinate lines_intercept(
+//    const RealCoordinate& l1a, const RealCoordinate& l1b, 
+//    const RealCoordinate& l2a, const RealCoordinate& l2b
+//);
 
-bool line_segments_intersect(
-    const RealCoordinate& l1a, const RealCoordinate& l1b,
-    const RealCoordinate& l2a, const RealCoordinate& l2b
-);
+//bool line_segments_intersect(
+//    const RealCoordinate& l1a, const RealCoordinate& l1b,
+//    const RealCoordinate& l2a, const RealCoordinate& l2b
+//);
 
-bool in_polygon(std::vector<RealCoordinate>& vertices, const RealCoordinate& c);
+//bool in_polygon(std::vector<RealCoordinate>& vertices, const RealCoordinate& c);
 
-bool num_is_between(double x, double a, double b);
+//bool num_is_between(double x, double a, double b);
 
-RealCoordinate midpoint(const RealCoordinate& a, const RealCoordinate& b); 
+//RealCoordinate midpoint(const RealCoordinate& a, const RealCoordinate& b); 
 
 std::vector<double> quadratic_roots(double a, double b, double c);
 
